6ad6fccad79b50ef01462bd98e152415d7ac2d4f
egg82 <phantom_zero@ymail.com>
2016-06-03 04:04:18 -0600

    Fixed permissions

--- src/me/egg82/tcpp/enums/PermissionsType.java
+++ src/me/egg82/tcpp/enums/PermissionsType.java
@@ -2,28 +2,28 @@ package me.egg82.tcpp.enums;
 
 public class PermissionsType {
 	//vars
-	public static final String IMMUNE = "immune";
-	public static final String COMMAND_TROLL = "command.troll";
-	public static final String COMMAND_VAPORIZE = "command.vaporize";
-	public static final String COMMAND_LIFT = "command.lift";
-	public static final String COMMAND_CREEP = "command.creep";
-	public static final String COMMAND_SWAP = "command.swap";
-	public static final String COMMAND_ENTOMB = "command.entomb";
-	public static final String COMMAND_SLOWPOKE = "command.slowpoke";
-	public static final String COMMAND_ELECTRIFY = "command.electrify";
-	public static final String COMMAND_ZOMBIFY = "command.zombify";
-	public static final String COMMAND_SLAP = "command.slap";
-	public static final String COMMAND_CANNON = "command.cannon";
-	public static final String COMMAND_BANISH = "command.banish";
-	public static final String COMMAND_BOMB = "command.bomb";
-	public static final String COMMAND_FREEZE = "command.freeze";
-	public static final String COMMAND_COMET = "command.comet";
-	public static final String COMMAND_STAMPEDE = "command.stampede";
-	public static final String COMMAND_HAUNT = "command.haunt";
-	public static final String COMMAND_LURE = "command.lure";
-	public static final String COMMAND_GARBLE = "command.garble";
-	public static final String COMMAND_WEAKLING = "command.weakling";
-	public static final String COMMAND_SPIN = "command.spin";
+	public static final String IMMUNE = "tcpp.immune";
+	public static final String COMMAND_TROLL = "tcpp.command.troll";
+	public static final String COMMAND_VAPORIZE = "tcpp.command.vaporize";
+	public static final String COMMAND_LIFT = "tcpp.command.lift";
+	public static final String COMMAND_CREEP = "tcpp.command.creep";
+	public static final String COMMAND_SWAP = "tcpp.command.swap";
+	public static final String COMMAND_ENTOMB = "tcpp.command.entomb";
+	public static final String COMMAND_SLOWPOKE = "tcpp.command.slowpoke";
+	public static final String COMMAND_ELECTRIFY = "tcpp.command.electrify";
+	public static final String COMMAND_ZOMBIFY = "tcpp.command.zombify";
+	public static final String COMMAND_SLAP = "tcpp.command.slap";
+	public static final String COMMAND_CANNON = "tcpp.command.cannon";
+	public static final String COMMAND_BANISH = "tcpp.command.banish";
+	public static final String COMMAND_BOMB = "tcpp.command.bomb";
+	public static final String COMMAND_FREEZE = "tcpp.command.freeze";
+	public static final String COMMAND_COMET = "tcpp.command.comet";
+	public static final String COMMAND_STAMPEDE = "tcpp.command.stampede";
+	public static final String COMMAND_HAUNT = "tcpp.command.haunt";
+	public static final String COMMAND_LURE = "tcpp.command.lure";
+	public static final String COMMAND_GARBLE = "tcpp.command.garble";
+	public static final String COMMAND_WEAKLING = "tcpp.command.weakling";
+	public static final String COMMAND_SPIN = "tcpp.command.spin";
 	
 	//constructor
 	public PermissionsType() {
